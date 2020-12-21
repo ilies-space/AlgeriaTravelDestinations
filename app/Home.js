32cb11c7de7b00a450b27cc31cda7ce727edefcb
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2020-12-21 19:51:58 +0100

    basic structure

---
+++ app/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={{flex: 1}}>
+      <Text>Home</Text>
+    </View>
+  );
+}
